a36f6de74f50b81052edb0f17c5fef5a613cd3e2
Carlos Silva <carlospxsilva@gmail.com>
2022-10-29 07:13:10 -0300

    construindo o segundo crud

---
+++ src/main/java/com/educandoweb/curso/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Carlos", "carlos@gmail.com", "414123", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
